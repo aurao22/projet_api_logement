7fbd47e9fb740418107be68a36d1cdb98635a05f
Aurelie RAOUL <aurelie.raoul@yahoo.fr>
2022-05-05 17:21:17 +0200

    Nettoyage Flask

--- projet_api_logement_aurelie_FLASK/app_flask.py
+++ projet_api_logement_aurelie_FLASK/app_flask.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-import pandas as pd
-from api_logement_function import load_model
-from os import getcwd
+import sys
+sys.path.append("C:\\Users\\User\\WORK\\workspace-ia\\PROJETS\\")
+from projet_api_logement.api_logement_commons import do_prediction
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
@@ -34,39 +34,6 @@ def hello():
                         nb_bedroom_mean=nb_bedroom_mean, population=population, 
                         occupation_mean=occupation_mean, 
                         latitude=latitude, longitude=longitude)
-
-
-def do_prediction(revenu_median, age_median, nb_room_mean, nb_bedroom_mean, population, occupation_mean, latitude, longitude):
-    
-    # "MedInc	HouseAge	AveRooms	AveBedrms	Population	AveOccup	Latitude	Longitude"
-    input_datas = {
-        "MedInc":[revenu_median],
-        "HouseAge":[age_median],
-        "AveRooms":[nb_room_mean],
-        "AveBedrms":[nb_bedroom_mean],
-        "Population":[population],
-        "AveOccup":[occupation_mean],
-        "Latitude":[latitude],
-        "Longitude":[longitude]
-    }
-    df_to_predict = pd.DataFrame(input_datas)
-    
-    # Récupère le répertoire du programme
-    file_path = getcwd() + "\\"
-    print(f"Current execution path : {file_path}")
-    # C:\Users\User\WORK\workspace-ia\PROJETS\projet_api_logement\static\Forest_2022-05-05-10_29_03.joblib
-    model_path = file_path
-    if "PROJETS" not in model_path:
-        model_path = model_path + r'PROJETS\projet_api_logement'
-    elif "projet_api_logement" not in model_path:
-        model_path = model_path + r'projet_api_logement'
-    model_path = model_path + r'/static/Forest_2022-05-05-10_29_03.joblib'
-    # Chargement du model
-    model = load_model(model_path)
-    predict_price = model.predict(df_to_predict)
-    # Attention, c'est n numpy.ndarray qui est retourné
-    print(f"Prédiction : {predict_price}")
-    return predict_price[0]
 
 
 if __name__ == "__main__":
---
+++ projet_api_logement_aurelie_mlflow/app_ml_flow.py
@@ -0,0 +1,22 @@
+# Using the Tracking API
+from mlflow import log_metric, log_param, log_artifact
+import sys
+sys.path.append("C:\\Users\\User\\WORK\\workspace-ia\\PROJETS\\")
+from projet_api_logement.api_logement_commons import do_prediction
+
+
+
+
+if __name__ == "__main__":
+    # Log a parameter (key-value pair)
+    log_param("param1", 5)
+
+    # Log a metric; metrics can be updated throughout the run
+    log_metric("foo", 1)
+    log_metric("foo", 2)
+    log_metric("foo", 3)
+
+    # Log an artifact (output file)
+    with open("output.txt", "w") as f:
+        f.write("Hello world!")
+    log_artifact("output.txt")
