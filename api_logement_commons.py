a9d0c3f1e8dc2453b02532baa2c4faa23a9f460b
Aurelie RAOUL <aurelie.raoul@yahoo.fr>
2022-05-19 11:35:52 +0200

    get_python_version

--- api_logement_commons.py
+++ api_logement_commons.py
@@ -70,3 +70,10 @@ def get_img_path():
     
     print(f"IMG path : {model_path}")
     return model_path
+
+def get_python_version():
+    from sys import version_info
+    PYTHON_VERSION = "{major}.{minor}.{micro}".format(major=version_info.major,
+                                              minor=version_info.minor,
+                                              micro=version_info.micro)
+    return PYTHON_VERSION
